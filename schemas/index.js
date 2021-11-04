d07dad667f7195160fa0b0482ad5e7820140c954
kkIIun <rlwjd4177@naver.com>
2021-11-04 15:10:44 +0900

    fix: mogodb deploy

--- schemas/index.js
+++ schemas/index.js
@@ -13,37 +13,37 @@ const connect = () => {
   //   ":" +
   //   process.env.password +
   //   "@localhost:27017/admin";
-  mongoose.connect(
-    uri,
-    {
-      dbName: "MP_prj",
-    },
-    (error) => {
-      if (error) {
-        console.log("몽고디비 연결 에러", error);
-      } else {
-        console.log("몽고디비 연결 성공");
-      }
-    }
-  );
+  // mongoose.connect(
+  //   uri,
+  //   {
+  //     dbName: "MP_prj",
+  //   },
+  //   (error) => {
+  //     if (error) {
+  //       console.log("몽고디비 연결 에러", error);
+  //     } else {
+  //       console.log("몽고디비 연결 성공");
+  //     }
+  //   }
+  // );
 
-  // const { MongoClient } = require("mongodb");
-  // const uri =
-  //   "mongodb+srv://rlwjd4177:somuch0503@cluster0.1mhao.mongodb.net/MP_prj?retryWrites=true&w=majority";
-  // const client = new MongoClient(uri, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // });
-  // client.connect((err) => {
-  //   const collection = client.db("test").collection("devices");
-  //   // perform actions on the collection object
-  //   client.close();
-  // });
+  const { MongoClient } = require("mongodb");
+  const uri =
+    "mongodb+srv://rlwjd4177:somuch0503@cluster0.1mhao.mongodb.net/MP_prj?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
 
-  mongoose.connection.on("error", (error) => {
+  client.on("error", (error) => {
     console.log("몽고디비 연결 에러", error);
   });
-  mongoose.connection.on("disconnected", () => {
+  client.on("disconnected", () => {
     console.log("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
     connect();
   });
